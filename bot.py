98fef423448345a393897c6a1932f57f7b21621a
Alexander LeBouthillier <45981454+alebouthil@users.noreply.github.com>
2023-01-14 14:55:30 -0500

    Playlist suport
    added playlist support, as well as new commands for stop and skip

--- bot.py
+++ bot.py
@@ -17,7 +17,12 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
+#audio options
+FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+playlist = []
 
 @bot.command(name = 'play', help = "Search youtube for the phrase given, play first result")
 async def play(ctx, song):
@@ -39,19 +44,23 @@ async def play(ctx, song):
 
     #get youtube video information
     video = songRequest(song)
-
-    #inform user what song is playing
-    await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
     
     #use pafy to get audio only stream of youtube video
     link = ("https://www.youtube.com/watch?v=" + video["items"][0]["id"]["videoId"])
     pafyVideo = pafy.new(link)
     stream = pafyVideo.getbestaudio()
 
+    if (botVoice.is_playing()):
+        print("queuing new song")
+        playlist.append((video, stream))
+        await ctx.send("Added " + video["items"][0]["snippet"]["title"] + " to queue")
+
     #play video audio over voice channel
-    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS), after=lambda e: print('done', e))
+    botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS), after=lambda e: playNext(e, ctx, botVoice, playlist))
     botVoice.is_playing()
+
+    #inform user what song is playing
+    await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
 
 @bot.command(name = 'playUrl', help = "Play the given url")
 async def playUrl(ctx, url):
@@ -70,6 +79,79 @@ async def playUrl(ctx, url):
         await botVoice.move_to(voiceChannel)
     else:
         botVoice = await voiceChannel.connect() 
+    
+    #use pafy to get best audio stream from input url
+    pafyVideo = pafy.new(url)
+    stream = pafyVideo.getbestaudio()
+
+    if (botVoice.is_playing()):
+        #queue song request if one is already playing
+        print("queuing new song")
+        await ctx.send("Added " + url + " to queue")
+    else:
+        #play video audio over voice channel
+        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS), after=lambda e: playNext(e, ctx, botVoice, playlist))
+        botVoice.is_playing()
+
+        #inform user what song is playing
+        await ctx.send("Now playing: " + url)
+
+@bot.command(name = "stop", help = "stop current song and deletes queue")
+async def stop(ctx):
+
+    #get voice client
+    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    #check if playing, stop if true
+    if not botVoice.is_playing():
+        await ctx.send("I'm not playing anything!")
+    else:
+        await ctx.send("Party pooper")
+        botVoice.stop()
+
+@bot.command(name = "skip", help = "skips current song and moves to next in queue")
+async def skip(ctx, playlist):
+
+    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    if playlist == []:
+        await ctx.send("No other songs in queue")
+    elif not botVoice.is_playing():
+        await ctx.send("Not currently playing")
+    else:
+        video = playlist.pop(0)
+        stream = video[1]
+        video = video[0]
+
+        botVoice.stop()
+        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: playNext(e, ctx, botVoice, playlist))
+        botVoice.is_playing()
+
+
+#internal function to allow playlist support
+async def playNext(e, ctx, botVoice, playlist):
+
+    #log any errors from previous song
+    print("errors: " + e)
+
+    #check if any songs are in queue, disconnect if not
+    if playlist == []:
+        await ctx.send("Queue finished!")
+        await botVoice.disconnect()
+    else:
+        #extract enqueued song
+        video = playlist.pop(0)
+        stream = video[1]
+        video = video[0]
+        
+        print("playing next")
+
+        #play video audio over voice channel
+        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: playNext(e, ctx, botVoice, playlist))
+        botVoice.is_playing()
+
+        #inform user what song is playing
+        await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
 
 #start bot
 bot.run(TOKEN)
