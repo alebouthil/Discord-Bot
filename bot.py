b28600197735a245b426a9e28e160615f6c97cf9
Alexander LeBouthillier <45981454+alebouthil@users.noreply.github.com>
2023-04-07 00:17:36 -0400

    Discord.py 2.2.2 Update and code refactor
    Code refactored and functionality improved
    youtube-dl replaced with yt-dlp to allow for streaming of age restricted videos
    playlist functionality improved with new functions playSong(), queueString() and queueUrl()
    added diagnostics command for server members.

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@ from ast import alias
 import os
 import discord
 import pafy
-from youtube_dl import YoutubeDL
 import asyncio
 
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import get
 from youtubeIntegration import songRequest
-from time import sleep
 
 #load discord bot token from environment variable
 load_dotenv()
@@ -22,157 +20,175 @@ intents.message_content = True
 #audio options
 FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
+#initiate bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+#initiate global playlist queue
 playlist = []
 
-@bot.command(name = 'play', help = "Search youtube for the phrase given, play first result")
+#primary command, user requests a song by search term or link
+@bot.command(name = 'play', help = "Search youtube for the phrase given, play first result", aliases = ["p", "Play", "PLAY"])
 async def play(ctx,*, song):
 
-    #check that author voice channel exists 
-    if ctx.author.voice is None:
+    #check that author voice channel exists, try to join if so
+    if ctx.author.voice.channel is None:
         await ctx.send("Please join a voice channel first!")
         print("User attempted play when not in voice channel")
         return
     else:
-        voiceChannel = ctx.author.voice.channel
+        voiceChannel = ctx.message.author.voice.channel
+        try:
+        #try creates voice client, except moves voice client if already created on server but in different channel
+            botVoice = await voiceChannel.connect()
+        except:
+            botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+            if botVoice.channel != voiceChannel:
+                await botVoice.move_to(voiceChannel)
 
-    #check if bot is already in a voice channel, join author voice channel if not
-    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if botVoice and botVoice.is_connected():
-        await botVoice.move_to(voiceChannel)
-    else:
-        botVoice = await voiceChannel.connect() 
+        #Queue video given, different functions for search term vs url
+        if song.startswith("http"):
+            await queueUrl(song, ctx)
+        else:
+            await queueString(song, ctx)
 
-    #get youtube video information
+        if not botVoice.is_playing():
+            skipInvoke = 0
+            await playSong(botVoice, ctx, skipInvoke)
+            
+async def queueUrl(song, ctx):
+#auxillary function to queue song from a link
+
+    #check if url is a youtube short, convert to video if so
+    if song.startswith("https://www.youtube.com/shorts/"):
+        urlSplit = song.split("/")
+        id = urlSplit[4]
+        print(id)
+        song = "https://www.youtube.com/watch?v=" + id
+
+    #extract audio stream from youtube link
+    pafyVideo = pafy.new(song)
+    stream = pafyVideo.getbestaudio()
+    video = {'items': [{'id': {'videoId': 'fecSD7CgxAI'}, 'snippet': {'publishedAt': '2023-01-23T07:43:18Z', 'channelId': 'UCeZoKzVi_FCb9a-96XqU5iQ', 'title': song, 'description': 'countrymusic #countrynews #topcountrysongs Top 100 Country Songs of 2023  Chris Lane, Morgan Wallen, Luke Combs, Chris ...', 'channelTitle': 'Country Music Collection'}}]}
+    duration = pafyVideo.length
+
+    print("Queued Url: " + song)
+
+    #add video information and stream to playlist queue
+    playlist.append((song, stream, duration))
+
+    await ctx.send("Added " + song + " to queue")
+
+async def queueString(song, ctx):
+#auxillary function to queue song from a search term
+
+    #search for request on Youtube
     video = songRequest(song)
 
-    print(video)
-    
-    #use pafy to get audio only stream of youtube video
+    #extract audio stream from youtube page
     link = ("https://www.youtube.com/watch?v=" + video["items"][0]["id"]["videoId"])
     pafyVideo = pafy.new(link)
     stream = pafyVideo.getbestaudio()
+    duration = pafyVideo.length
 
-    if (botVoice.is_playing()):
-        print("queuing new song")
-        playlist.append((video, stream))
-        await ctx.send("Added " + video["items"][0]["snippet"]["title"] + " to queue")
+    print("Queueing: " + video["items"][0]["snippet"]["title"])
 
-    #play video audio over voice channel
-    botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS), after=lambda e: print('done', e))
-    botVoice.is_playing()
+    #add video information and stream to playlist queue
+    playlist.append((video["items"][0]["snippet"]["title"], stream, duration))
 
-    #inform user what song is playing
-    await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
+    await ctx.send("Added " + video["items"][0]["snippet"]["title"] + " to queue")
 
-    await playNext(ctx, botVoice)
+async def playSong(botVoice, ctx, skipInvoke):
+#auxillary function that manages song plays and playlist support
 
-@bot.command(name = 'playUrl', help = "Play the given url")
-async def playUrl(ctx, url):
+    if (skipInvoke == 1):
+    #causes function to play next song in queue
+        botVoice.stop()
 
-    #check that author voice channel exists 
-    if ctx.author.voice is None:
-        await ctx.send("Please join a voice channel first!")
-        print("User attempted play when not in voice channel")
+        #extract song information from playlist queue
+        song = playlist.pop(0)
+        title = song[0]
+        stream = song[1]
+        duration = song[2]
+
+        #play song
+        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: print('done', e))
+        botVoice.is_playing()
+
+        await ctx.send("Skipped! Now playing: " + title)
+
+        #play next song, without skipping, after sleeping currenmt songs duration
+        await asyncio.sleep(duration + 1)
+        skipInvoke = 0
+        await playSong(botVoice, ctx, skipInvoke)
         return
-    else:
-        voiceChannel = ctx.author.voice.channel
 
-    #check if bot is already in a voice channel, join author voice channel if not
-    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if botVoice and botVoice.is_connected():
-        await botVoice.move_to(voiceChannel)
-    else:
-        botVoice = await voiceChannel.connect() 
+    elif playlist != []:
+    #default function behaviour, starts playlist and continues until empty
+
+        botVoice.stop()
+
+        #extract song information from playlist
+        song = playlist.pop(0)
+        title = song[0]
+        stream = song[1]
+        duration = song[2]
+
+        #play song
+        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: print('done', e))
+        botVoice.is_playing()
+
+        await ctx.send("Now playing: " + title)
+
+        #sleep for song duration, play next
+        await asyncio.sleep(duration + 1)
+        await playSong(botVoice, ctx, skipInvoke)
+        return
     
-    #use pafy to get best audio stream from input url
-    pafyVideo = pafy.new(url)
-    stream = pafyVideo.getbestaudio()
-
-    video = {'items': [{'id': {'videoId': 'fecSD7CgxAI'}, 'snippet': {'publishedAt': '2023-01-23T07:43:18Z', 'channelId': 'UCeZoKzVi_FCb9a-96XqU5iQ', 'title': url, 'description': 'countrymusic #countrynews #topcountrysongs Top 100 Country Songs of 2023  Chris Lane, Morgan Wallen, Luke Combs, Chris ...', 'channelTitle': 'Country Music Collection'}}]}
-
-    if (botVoice.is_playing()):
-        print("queuing new song")
-        playlist.append((video, stream))
-        await ctx.send("Added " + video["items"][0]["snippet"]["title"] + " to queue")
-
-    #play video audio over voice channel
-    botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS), after=lambda e: print('done', e))
-    botVoice.is_playing()
-
-    #inform user what song is playing
-    await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
-
-    await playNext(ctx, botVoice)
+    else:
+        await ctx.send("Playlist empty")
+        return
 
 @bot.command(name = "stop", help = "stop current song and deletes queue")
 async def stop(ctx):
 
-    #get voice client
-    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    #check if playing, stop if true
-    if not botVoice.is_playing():
+    #Check if voice client exists, stops playback if currently playing
+    try:
+        botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    except:
         await ctx.send("I'm not playing anything!")
     else:
-        await ctx.send("Party pooper")
-        botVoice.stop()
+        if (botVoice.is_playing()):
+            botVoice.stop()
+            await ctx.send(ctx.message.author.name + " has stopped the music :(")
     
     #deletes queue
     playlist = []
 
-@bot.command(name = "skip", help = "skips current song and moves to next in queu. Aliases: !next", aliases = ["next"])
+@bot.command(name = "skip", help = "skips current song and moves to next in queue. Aliases: !next", aliases = ["next"])
 async def skip(ctx):
 
-    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if playlist == []:
-        await ctx.send("No other songs in queue")
-    elif not botVoice.is_playing():
+    try:
+        botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    except:
         await ctx.send("Not currently playing")
     else:
-        video = playlist.pop(0)
-        stream = video[1]
-        video = video[0]
+        if playlist == []:
+            await ctx.send("No other songs in queue")
+        else:
+            skipInvoke = 1
+            await playSong(botVoice, ctx, skipInvoke)
 
-        await ctx.send("Skipped")
+@bot.command(name = "Diagnostics", help = "Display bot metrics to users")
+async def diagnostics(ctx):
+#function to allow for bot diagnostics by server users
 
-        botVoice.stop()
-        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: print('done', e))
-        botVoice.is_playing()
-
-        await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
-
-        await playNext(ctx, botVoice)
-
-
-#internal function to allow playlist support
-async def playNext(ctx, botVoice):
-
-    while botVoice.is_playing():
-        await asyncio.sleep(.1)
-
-    #check if any songs are in queue, disconnect if not
-    if playlist == []:
-        await ctx.send("Queue finished!")
-        #await botVoice.disconnect()
-    else:
-        #extract enqueued song
-        video = playlist.pop(0)
-        stream = video[1]
-        video = video[0]
-        
-        print("playing next")
-
-        #play video audio over voice channel
-        botVoice.play(discord.FFmpegPCMAudio(stream.url, **FFMPEG_OPTS),after=lambda e: print('done', e))
-        botVoice.is_playing()
-
-        #inform user what song is playing
-        await ctx.send("Now playing: " + video["items"][0]["snippet"]["title"])
-
-        await playNext(ctx, botVoice)
+    botVoice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    await ctx.send("Voice connection: ") 
+    await ctx.send(botVoice.is_connected())
+    await ctx.send("Voice channel: " + botVoice.channel)
+    await ctx.send("Current Queue length: " + playlist.count)
+    await ctx.send("Currently playing: ")
+    await ctx.send(botVoice.is_playing())
 
 #start bot
 bot.run(TOKEN)
